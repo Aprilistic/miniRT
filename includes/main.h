d70a08dd63a76fffd8e5db6b66dd6828a4a582d5
Jinho Heo <uj07077@gmail.com>
2023-02-18 21:13:28 +0900

    code refactoriztion

--- includes/main.h
+++ includes/main.h
@@ -18,5 +18,4 @@
 # include "ray.h"
 # include <stdio.h>
 
-
 #endif
--- includes/shape.h
+++ includes/shape.h
@@ -1,9 +1,8 @@
 #ifndef SHAPE_H
 # define SHAPE_H
-
 # include "color.h"
 # include "ray.h"
-#include "vec3.h"
+# include "vec3.h"
 
 enum e_type
 {
@@ -18,21 +17,18 @@ typedef struct s_cone
 	t_point3	point;
 	double		height;
 	double		diameter;
-	t_color		color;
 }	t_cone;
 
 typedef struct s_plane
 {
 	t_point3	point;
 	t_vec3		dir_vector;
-	t_color		color;
 }	t_plane;
 
 typedef struct s_sphere
 {
 	t_point3	center;
 	double		diameter;
-	t_color		color;
 }	t_sphere;
 
 typedef struct s_cylinder
@@ -41,14 +37,13 @@ typedef struct s_cylinder
 	t_vec3		dir_vector;
 	double		diameter;
 	double		height;
-	t_color		color;
 }	t_cylinder;
 
 typedef struct s_shape
 {
 	int		type;
+	int		option;
 	t_color	color;
-	int		(*hit)(t_ray *ray, double tmin, double tmax);
 	void	*characteristic;
 }	t_shape;
 
