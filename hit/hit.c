0d92f807bbe1768cfac80cb1f0c2ba399fb47b56
Jinho Heo <uj07077@gmail.com>
2023-02-26 20:45:36 +0900

    hit_by_plane formula error fixed, need to test non horizontal planes

--- hit/hit.c
+++ hit/hit.c
@@ -48,28 +48,28 @@ int	hit_by_sphere(t_ray ray, t_object *object, t_record *hit_record)
 	return (1);
 }
 
-void	make_right_normal(t_ray *ray, t_plane *plane)
-{
-	// 여기서 법선 벡터를 올바르게 만들어야 한다.
-	if (plane->normal.e[1] == 1 || plane->normal.e[1] == -1)
-	{
-		// 카메라의 위치가 면의 점보다 아래면
-		if (ray->origin.e[1] < plane->point.e[1])
-			plane->normal.e[1] = -1;
-		else
-			plane->normal.e[1] = 1;
-	}
-	else if (plane->normal.e[2] == 1 || plane->normal.e[2] == -1)
-	{
-		if (ray->dir.e[2] >= EPSILON && plane->normal.e[2] >= EPSILON)
-		{
-			if (plane->normal.e[2] == 1)
-				plane->normal.e[2] = -1;
-			else
-				plane->normal.e[2] = 1;
-		}
-	}
-}
+// void	make_right_normal(t_ray *ray, t_plane *plane)
+// {
+// 	// 여기서 법선 벡터를 올바르게 만들어야 한다.
+// 	if (plane->normal.e[1] == 1 || plane->normal.e[1] == -1)
+// 	{
+// 		// 카메라의 위치가 면의 점보다 아래면
+// 		if (ray->origin.e[1] < plane->point.e[1])
+// 			plane->normal.e[1] = -1;
+// 		else
+// 			plane->normal.e[1] = 1;
+// 	}
+// 	else if (plane->normal.e[2] == 1 || plane->normal.e[2] == -1)
+// 	{
+// 		if (ray->dir.e[2] >= EPSILON && plane->normal.e[2] >= EPSILON)
+// 		{
+// 			if (plane->normal.e[2] == 1)
+// 				plane->normal.e[2] = -1;
+// 			else
+// 				plane->normal.e[2] = 1;
+// 		}
+// 	}
+// }
 
 int	hit_by_plane(t_ray ray, t_object *object, t_record *hit_record)
 {
@@ -80,14 +80,15 @@ int	hit_by_plane(t_ray ray, t_object *object, t_record *hit_record)
 	t_plane		*plane;
 
 	plane = (t_plane *)(object)->equation;
-	make_right_normal(&ray, plane);
+	// make_right_normal(&ray, plane);
 	// 수직이거나 안 만날 때
 	discriminant = v_dot(plane->normal, ray.dir);
-	if (discriminant >= EPSILON)
+	if (fabs(discriminant) < EPSILON)
 		return (0);
 	root = v_dot(v_sub(plane->point, ray.origin), plane->normal) / discriminant;
 	hit_record->origin = v_add(ray.origin, v_mul_scalar(ray.dir, root));
 	hit_record->normal = plane->normal;
+	set_face_normal(ray, hit_record);
 	hit_record->suface = object->surface;
 	return (1);
 }
