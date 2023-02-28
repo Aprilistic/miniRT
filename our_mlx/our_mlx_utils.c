df65a16b60efbe0bc5a9259e394459f91cb78dc5
Jinho Heo <uj07077@gmail.com>
2023-02-28 22:12:39 +0900

    camera fov optimazation added

--- our_mlx/our_mlx_utils.c
+++ our_mlx/our_mlx_utils.c
@@ -7,9 +7,11 @@ int	mouse_hook(int mouse_code, int x, int y, t_mlx *mlx)
 	y = 0;
 	if (mouse_code == SCROLL_UP || mouse_code == SCROLL_DOWN)
 	{
-		camera_fov_change(mlx, mouse_code);
-		mlx_clear_window(mlx->mlx_ptr, mlx->win_ptr);
-		make_image(mlx);
+		if (camera_fov_change(mlx, mouse_code))
+		{
+			mlx_clear_window(mlx->mlx_ptr, mlx->win_ptr);
+			make_image(mlx);
+		}
 	}
 	return (0);
 }
