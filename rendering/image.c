61156498c73bfb65a077dbe7193a4272a787408f
Jinho Heo <uj07077@gmail.com>
2023-03-01 13:26:10 +0900

    formula optimization and norm fix

--- rendering/image.c
+++ rendering/image.c
@@ -14,15 +14,15 @@ double	degree_to_radian(double degree)
 	return (degree * PI / 180);
 }
 
-t_vec3	cam_offset_vector(t_mlx *mlx, double width, int x, int y)
+t_vec3	cam_offset_vector(t_mlx *mlx, double factor, int x, int y)
 {
 	double	x_scale;
 	double	y_scale;
 	t_vec3	x_dir;
 	t_vec3	y_dir;
 
-	x_scale = width / (X_SIZE) * (x * 2 - X_SIZE);
-	y_scale = width / X_SIZE * (y * 2 - Y_SIZE);
+	x_scale = factor * (x * 2 - X_SIZE);
+	y_scale = factor * (y * 2 - Y_SIZE);
 	x_dir = v_mul_scalar(mlx->camera.u, x_scale);
 	y_dir = v_mul_scalar(mlx->camera.v, y_scale);
 	return (v_add(x_dir, y_dir));
@@ -33,9 +33,9 @@ void	make_image(t_mlx *mlx)
 	int		x;
 	int		y;
 	t_ray	pixel;
-	double	width;
+	double	factor;
 
-	width = tan(degree_to_radian(mlx->camera.fov / 2.0));
+	factor = tan(degree_to_radian(mlx->camera.fov / 2.0)) / X_SIZE;
 	y = -1;
 	while (++y < Y_SIZE)
 	{
@@ -44,7 +44,7 @@ void	make_image(t_mlx *mlx)
 		{
 			pixel.origin = mlx->camera.origin;
 			pixel.dir = v_add(mlx->camera.dir,
-					cam_offset_vector(mlx, width, x, y));
+					cam_offset_vector(mlx, factor, x, y));
 			pixel.dir = v_unit(pixel.dir);
 			my_mlx_pixel_put(
 				&mlx->img, x, Y_SIZE - 1 - y, create_rgb(ray_color(pixel,
